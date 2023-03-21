9e1fbff9f7086c95eabadb8784fd68a270057dce
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-03-21 19:26:19 +0200

    Just newbie exercises

---
+++ DataTypesVariables.py
@@ -0,0 +1,50 @@
+# coffe_counter = 0
+#
+# while True:
+#     line = input()
+#     if line == "END":
+#         break
+#
+#     if line == "coding" or line == "cat" or line =="dog" or line == "movie":
+#         coffe_counter+=1
+#     elif line == "CODING" or line == "CAT" or line =="DOG" or line == "MOVIE":
+#         coffe_counter += 2
+#
+# if coffe_counter>5:
+#     print("You need extra sleep")
+# else:
+#     print(coffe_counter)
+
+# n = int(input())
+# m= n / 1000
+#
+# print(f"{m:.2f}")
+# brit=int(input())
+# usd = brit*1.31
+#
+# print(f"{usd:.3f}")
+
+# centuries=int(input())
+#
+# year=centuries*100
+# days = int(year*365.2422)
+# hour = days * 24
+# minutes = hour*60
+#
+# print(f"{centuries} centuries = {year} years = {days} days = {hour} hours = {minutes} minutes")
+
+n = int(input())
+
+# for num in range(1,n+1):
+#     is_special=False
+#     num_as_str=str(num)
+#     sum_digit=0
+#     for char in num_as_str:
+#         sum_digit += int(char)
+#         if sum_digit == 5 or sum_digit == 7 or sum_digit == 11:
+#             is_special = True
+#     print(f"{num} -> {is_special}")
+
+
+
+
