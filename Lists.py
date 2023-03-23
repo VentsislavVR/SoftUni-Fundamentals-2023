0270789af344fdeeebd5143978fedcc104e66404
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-03-23 11:00:37 +0200

    Just newbie exercises /// tasks 1 to 6 // I will probably just # them

--- Lists.py
+++ Lists.py
@@ -1,7 +1,22 @@
-tail = input()
-body = input()
-head = input()
 
-zoo_list=[head,body,tail]
+# FIRST
+# tail = input()
+# body = input()
+# head = input()
+#
+# zoo_list=[head,body,tail]
+#
+# print(zoo_list)
 
-print(zoo_list)
+#SECOND
+
+n = int(input())
+courses=[]
+for _ in range(n):
+    course_name=input()
+    courses.append(course_name)
+
+print(courses)
+
+
+
