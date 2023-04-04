c87d4032eb5c09090038772cf8d897cb38a9af4e
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-04-04 12:12:48 +0300

    ListEx 3/10

---
+++ ListEx.py
@@ -0,0 +1,27 @@
+#   FIRST
+# input_elements = input().split()
+#
+# result = []
+#
+# for el in input_elements:
+#     number = int(el)
+#     result.append(-number)
+# print(result)
+
+#  SECOND
+# factor = int(input())
+# count = int(input())
+#
+# result = []
+# for num in range(1,count+1):
+#     result.append(num*factor)
+#
+# print(result)
+#  THIRD
+
+
+
+
+
+
+
