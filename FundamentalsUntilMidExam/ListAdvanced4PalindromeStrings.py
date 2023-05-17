9d10d7eee57193c4496ac3d6b6091d6b0e934565
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-05-17 20:58:16 +0300

    Merge remote-tracking branch 'origin/main'

--- FundamentalsUntilMidExam/ListAdvanced4PalindromeStrings.py
+++ FundamentalsUntilMidExam/ListAdvanced4PalindromeStrings.py
@@ -2,6 +2,7 @@ words = input().split()
 searched_palindrome = input()
 
 print([word for word in words if word == word[::-1]])
-print(words.count(searched_palindrome))
+print(f"Found palindrome {words.count(searched_palindrome)} times")
+
 
 
