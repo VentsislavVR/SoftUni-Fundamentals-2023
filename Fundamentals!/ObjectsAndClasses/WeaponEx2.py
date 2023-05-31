915a00c71daeacfcee4276038ddc51928ca5427b
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-05-31 22:40:07 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ Fundamentals!/ObjectsAndClasses/WeaponEx2.py
@@ -0,0 +1,57 @@
+class Weapon:
+    def __init__(self, bullets):
+        self.bullets = bullets
+
+    def shoot(self):
+        if 0 < self.bullets:
+            self.bullets -= 1
+            return "shooting..."
+        else:
+            return "no bullets left"
+
+    def __repr__(self):
+        return f"Remaining bullets: {self.bullets}"
+
+
+weapon = Weapon(34)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon)
+print(weapon.shoot())
+print(weapon.shoot())
+print(weapon.shoot())
