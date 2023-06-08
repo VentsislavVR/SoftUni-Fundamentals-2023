94b898ce86660a7d920a178695e3260ecf2d5f5f
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-06-08 21:57:56 +0300

    Merge remote-tracking branch 'origin/main'

--- Fundamentals!/Dictionaries/StockLab2.py
+++ Fundamentals!/Dictionaries/StockLab2.py
@@ -1,3 +1,15 @@
-beautifull = {"красив" : ['pretty',"nice","good"]}
+products = {}
+data = input().split()
 
-print(beautifull["красив"])
+for index in range(0, len(data), 2):
+    key = data[index]
+    value = data[index + 1]
+    products[key] = value
+
+searched_products = input().split()
+
+for searched_product in searched_products:
+    if searched_product in products.keys():
+        print(f"We have {products[searched_product]} of {searched_product} left")
+    else:
+        print(f"Sorry, we don't have {searched_product}")
