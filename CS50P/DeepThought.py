7faea8111f0fe4c4aabae37cbb09d0b6a5ef5f2f
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-06-08 23:35:12 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ CS50P/DeepThought.py
@@ -0,0 +1,6 @@
+
+z = input("What is the Answer to the Great Question of Life... the Universe... and Everything?").lower().strip()
+if z == "42" or z == "forty-two" or z == "forty two":
+    print("Yes")
+else:
+    print("No")
