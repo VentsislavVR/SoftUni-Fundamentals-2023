e146be3d74294f077adbbfb3fba60397e833a03a
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-06-23 18:55:12 +0300

    Merge remote-tracking branch 'origin/main'

--- Fundamentals!/Dictionaries/Odd OccurrencesLab6.py
+++ Fundamentals!/Dictionaries/Odd OccurrencesLab6.py
@@ -1,3 +1,19 @@
+# word_keys = [el for el in input().lower().split(" ")]
+#
+# result_dict = {}
+# for element in word_keys:
+#     key = element
+#     value_start = 0
+#     if element not in result_dict:
+#         key = element
+#         result_dict[key] = value_start
+#         result_dict[key] += 1
+#     else:
+#         result_dict[key] +=1
+#
+# for key,value in result_dict.items():
+#     if value % 2 !=0:
+#         print(key,end=" ")
 # words_keys = [el.lower() for el in input().split()]
 # default = 0
 # occurrences = dict.fromkeys(words_keys,default)
