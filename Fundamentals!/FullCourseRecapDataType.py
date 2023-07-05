702c25775088bd8da34ba13c0f0eb783337a08a0
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-07-05 09:49:07 +0300

    Merge remote-tracking branch 'origin/main'

--- Fundamentals!/FullCourseRecapDataType.py
+++ Fundamentals!/FullCourseRecapDataType.py
@@ -27,7 +27,7 @@
 #     result+= ord(letter)
 #
 # print(f"The sum equals: {result}")
-#5
+# 5
 
 # start = int(input())
 # end = int(input())
@@ -35,14 +35,48 @@
 # for i in range(start,end+1):
 #     result += chr(i) + " "
 # print(result)
-#6th
+# 6th
 # num = int(input())
 #
 # for i in range(0,num):
 #     for j in range(0,num):
 #         for k in range(0,num):
 #             print(f"{chr(97+i)}{chr(97+j)}{chr(97+k)}")
+# capacity = 255
+# num_of_lines = int(input())
+#
+# current_volume = 0
+# for i in range(num_of_lines):
+#     adding = int(input())
+#     current_volume += adding
+#     if current_volume > capacity:
+#         print("Insufficient capacity!")
+#         current_volume-=adding
+# print(f"{current_volume}")
 
+# from math import floor
+# group_size = int(input())
+# days = int(input())
+#
+# coins = 0
+#
+# for day in range(1, days + 1):
+#     coins += 50
+#     coins -= group_size *2
+#
+#     if day % 15 == 0:
+#         group_size += 5
+#         coins -= group_size * 2
+#     if day % 10 == 0:
+#         group_size -= 2
+#     if day % 5 == 0:
+#         coins += group_size * 20
+#         # if day % 3 == 0:
+#         #     coins -= group_size * 2
+#     if day % 3 == 0:
+#         coins -= group_size * 3
+#
+# print(f"{group_size} companions received {floor(coins/group_size)} coins each.")
 
 
 
