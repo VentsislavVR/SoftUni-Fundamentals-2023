ced7e44703a19c4741852f99462a3cb1193d30a2
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-07-07 22:31:57 +0300

    Merge remote-tracking branch 'origin/main'

--- Fundamentals!/FinalExamPrep!!!/ImitationGame1Exam1.py
+++ Fundamentals!/FinalExamPrep!!!/ImitationGame1Exam1.py
@@ -2,14 +2,18 @@ def move_letters_to_back(string, n):
     if n >= len(string):
         return string  # No change needed if n is greater than or equal to string length
     return string[n:] + string[:n]
-def insert_func(string,idx,value):
+
+
+def insert_func(string, idx, value):
     if idx > len(string):
         return string
     return string[:index] + value + string[index:]
 
-def change_func(string,subs,newsubs):
+
+def change_func(string, subs, newsubs):
     if subs in string:
-        return string.replace(subs,newsubs)
+        return string.replace(subs, newsubs)
+
 
 result = ""
 while True:
@@ -20,16 +24,16 @@ while True:
     if command[0] == "Move":
         index = int(command[1])
         result = move_letters_to_back(result, index)
-        command[:index] = command[index:]
+
     elif command[0] == "Insert":
-         index = int(command[1])
-         value = command[2]
-         result = insert_func(result,index,value)
+        index = int(command[1])
+        value = command[2]
+        result = insert_func(result, index, value)
 
     elif command[0] == "ChangeAll":
-        substring= command[1]
+        substring = command[1]
         new = command[2]
-        result=change_func(result,substring,new)
+        result = change_func(result, substring, new)
     else:
         result += message
 
