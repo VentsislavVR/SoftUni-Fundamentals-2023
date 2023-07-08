19d65cda6144a865ce9f6cbcbfa9657425ef88d7
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-07-08 17:14:49 +0300

    Merge remote-tracking branch 'origin/main'

--- Fundamentals!/FinalExamPrep!!!/Test.py
+++ Fundamentals!/FinalExamPrep!!!/Test.py
@@ -1,11 +1,11 @@
-import re
-def check_groups(barcode):
-    group = re.findall(r"\d+", barcode)
-    digits = [int(x) for x in group]
-    if digits:
-        return print(f"Product group: {digits}")
-    return print(f"Product group: 00")
-
-bar = int
-barcode = input()
-check_groups(barcode)
+# import re
+# def check_groups(barcode):
+#     group = re.findall(r"\d+", barcode)
+#     digits = [int(x) for x in group]
+#     if digits:
+#         return print(f"Product group: {digits}")
+#     return print(f"Product group: 00")
+#
+# bar = int
+# barcode = input()
+# check_groups(barcode)
