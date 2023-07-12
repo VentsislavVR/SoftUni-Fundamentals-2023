685d5d3f704dfa8c8d41b12cd8f69bcbdc7ba943
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-07-12 14:50:55 +0300

    Merge remote-tracking branch 'origin/main'

--- Advanced/1.Lists asS tacks and Ques Lab/3.Supermarket Lab. Inespy.py
+++ Advanced/1.Lists asS tacks and Ques Lab/3.Supermarket Lab. Inespy.py
@@ -1,19 +1,39 @@
+# from collections import deque
+#
+#
+# customers = deque()
+# while True:
+#
+#     name = input()
+#     if name == "Paid":
+#         while customers:
+#             print(customers.popleft())
+#         continue
+#     elif name == "End":
+#         break
+#     customers.append(name)
+#
+# print(f"{len(customers)} people remaining.")
+
 from collections import deque
 
+class CustomerQueue:
+    def __init__(self):
+        self.customers = deque()
 
-customers = deque()
-while True:
+    def process_customers(self):
+        while True:
+            name = input()
+            if name == "Paid":
+                while self.customers:
+                    print(self.customers.popleft())
+                continue
+            elif name == "End":
+                break
+            self.customers.append(name)
 
-    name = input()
-    if name == "Paid":
-        while customers:
-            print(customers.popleft())
-        continue
-    elif name == "End":
-        break
-    customers.append(name)
-
-print(f"{len(customers)} people remaining.")
-
+        print(f"{len(self.customers)} people remaining.")
 
 
+queue = CustomerQueue()
+queue.process_customers()
--- Advanced/1.Lists asS tacks and Ques Lab/5.Hot Potato Ines.py
+++ Advanced/1.Lists asS tacks and Ques Lab/5.Hot Potato Ines.py
@@ -1,12 +1,39 @@
+# from collections import deque
+#
+# names = deque(input().split())
+#
+# rotation_num = int(input())-1
+#
+# while len(names) > 1:
+#     names.rotate(-rotation_num)
+#     exited_name = names.popleft()
+#     print(f"Removed {exited_name}")
+# print(f"Last is {names.popleft()}")
+
+
 from collections import deque
 
-names = deque(input().split())
+class NameGame:
+    def __init__(self):
+        self.names = deque()
 
-rotation_num = int(input())-1
+    def get_input(self):
+        name_list = input("Enter names (space-separated): ").split()
+        self.names.extend(name_list)
 
-while len(names) > 1:
-    names.rotate(-rotation_num)
-    exited_name = names.popleft()
-    print(f"Removed {exited_name}")
-print(f"Last is {names.popleft()}")
+    def play_game(self):
+        rotation_num = int(input("Enter rotation number: ")) - 1
+
+        while len(self.names) > 1:
+            self.names.rotate(-rotation_num)
+            exited_name = self.names.popleft()
+            print(f"Removed {exited_name}")
+
+        print(f"Last is {self.names.popleft()}")
+
+game = NameGame()
+game.get_input()
+game.play_game()
+
+
 
