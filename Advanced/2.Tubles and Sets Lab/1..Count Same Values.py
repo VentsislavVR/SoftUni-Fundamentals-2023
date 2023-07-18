49cf060370de8c4839d32d58f049658f89aca61f
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-07-18 09:15:24 +0300

    Merge remote-tracking branch 'origin/main'

--- Advanced/2.Tubles and Sets Lab/1..Count Same Values.py
+++ Advanced/2.Tubles and Sets Lab/1..Count Same Values.py
@@ -3,4 +3,4 @@ input_list = input().split()
 occurrences = {num: input_list.count(num) for num in input_list}
 
 for number,count in occurrences.items():
-    print(f"{number} - {count} times")
+    print(f"{float(number)} - {count} times")
---
+++ Advanced/3.Multidimensional Lists Lab/TEST.py
@@ -0,0 +1,8 @@
+# Python program to create a m x n matrix
+# with all 0s
+m = 4
+n = 5
+
+a = [[0 for x in range(n)] for x in range(m)]
+print(a)
+
