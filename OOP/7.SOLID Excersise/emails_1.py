52b5dbb2c706e05e469dd93d8d6e1a19b84336d8
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-08-11 10:05:23 +0300

    adding problems

--- OOP/7.SOLID Excersise/emails_1.py
+++ OOP/7.SOLID Excersise/emails_1.py
@@ -1,7 +1,25 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
-class IEmail(object):
-    __metaclass__ = ABCMeta
+
+class IContent(ABC):
+    def __init__(self, text):
+        self.text = text
+
+    @abstractmethod
+    def format(self):
+        ...
+
+
+class MyMl(IContent):
+    def format(self):
+        return '\n'.join(['<myML>', self.text, '</myML>'])
+
+class HTML(IContent):
+    def format(self):
+        return '\n'.join(['<html>', self.text, '</html>'])
+
+
+class IEmail(ABC):
 
     @abstractmethod
     def set_sender(self, sender):
@@ -15,11 +33,11 @@ class IEmail(object):
     def set_content(self, content):
         pass
 
+
 class Email(IEmail):
 
-    def __init__(self, protocol, content_type):
+    def __init__(self, protocol):
         self.protocol = protocol
-        self.content_type = content_type
         self.__sender = None
         self.__receiver = None
         self.__content = None
@@ -37,21 +55,17 @@ class Email(IEmail):
             self.__receiver = receiver
 
     def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+        self.__content = content.format()
 
     def __repr__(self):
 
-        template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
+        template = f"Sender: {self.__sender}\nReceiver: {self.__receiver}\nContent:\n{self.__content}"
 
-        return template.format(sender = self.__sender, receiver = self.__receiver, content = self.__content)
+        return template
 
-
-email = Email('IM', 'MyML')
+myml = MyMl("Hello, there")
+email = Email('IM')
 email.set_sender('qmal')
 email.set_receiver('james')
-email.set_content('Hello, there!')
+email.set_content(myml)
 print(email)
-
--- OOP/7.SOLID Excersise/prisoner_3.py
+++ OOP/7.SOLID Excersise/prisoner_3.py
@@ -1,9 +1,13 @@
 import copy
+from abc import ABC
+
 
 class Person:
-
     def __init__(self, position):
         self.position = position
+
+
+class FreePerson(Person):
 
     def walk_north(self, dist):
         self.position[1] += dist
@@ -16,9 +20,8 @@ class Prisoner(Person):
     PRISON_LOCATION = [3, 3]
 
     def __init__(self):
-        super(Prisoner, self).__init__(copy.copy(self.PRISON_LOCATION))
+        super().__init__(copy.copy(self.PRISON_LOCATION))
         self.is_free = False
-
 
 
 prisoner = Prisoner()
